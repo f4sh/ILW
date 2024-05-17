dc5d8b9a262124520da23ad2ee04e851eb5b789e
mistermatt1337 <127788067+mistermatt1337@users.noreply.github.com>
2024-05-17 16:21:43 +0000

    Add PWA Feature for web app and improved user experience

---
+++ sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"074f798645e5ae774559d21758d35574","url":"index.html"},{"revision":"6b96fd631c117b954bb140d9ce98dff4","url":"script.js"},{"revision":"6bf5c5be6d89478a761ed38b3e459e9d","url":"style.css"}]);
